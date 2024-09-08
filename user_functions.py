6a1233b1092fec20bb844ad7d752d4277ac40b97
Adam Duke <0soy.fish.studios0@gmail.com>
2024-09-08 18:03:06 +1000

    Create user_functions.py

---
+++ user_functions.py
@@ -0,0 +1,22 @@
+# creating your own functions in Python
+
+# create the function
+def HelloWorld():
+    print("Hello World")
+
+def PrintName(name):
+    print('Hi '+name+'!')
+
+def printSum(num1,num2):
+    print(num1+num2)
+
+def returnSum(num1,num2):
+    return(num1+num2)
+
+# call the function
+HelloWorld()
+PrintName("Adam")
+printSum(5,10)
+print(returnSum(5,10))
+
+# end of code
