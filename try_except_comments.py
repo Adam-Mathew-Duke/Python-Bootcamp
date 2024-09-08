d55a28d3ea2b28a3f7154acb2bc5b97b2462299b
Adam Duke <0soy.fish.studios0@gmail.com>
2024-09-08 17:36:30 +1000

    Create try_except_comments.py

---
+++ try_except_comments.py
@@ -0,0 +1,27 @@
+# try and except and comments
+# not sure if the results will be there
+# not sure if it will work
+# to handle errors
+
+try:
+    if name > 3: # error name is not defined
+        print("Hello")
+
+except:
+        print("Error") # catch the error
+
+# comments in Python
+
+# this is a single line comment in Python
+
+'''
+This is a multiple line comment
+As many lines as you like
+'''
+
+"""
+This is also
+a comment on multiple lines
+"""
+
+# end of code
